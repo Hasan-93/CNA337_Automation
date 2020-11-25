b1fb8990cf2d2d70883a2cb82d4b68f3fae5e0f1
Hasan-93 <62036798+Hasan-93@users.noreply.github.com>
2020-11-24 22:08:12 -0800

    Add files via upload

--- Server.py
+++ Server.py
@@ -1,10 +1,10 @@
+import os
 class Server:
     """ Server class for representing and manipulating servers. """
-
     def __init__(self, server_ip):
-        # TODO -
-        self.server_ip = server_ip
 
+        self.server_ip = server_ip
     def ping(self):
-        # TODO - Use os module to ping the server
-        return
+
+        response = os.system("ping -n 4 " +self.server_ip)
+        return self.server_ip
--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 # Zachary Rubin, zrubin@rtc.edu
 # CNA 337 Fall 2020
 
+from Server import Server
 def print_program_info():
-    # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
 
-# This is the entry point to our program
+    print("Server Automator v0.1 by Hasan")
+
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+
+    EC2server = Server('3.12.129.47')
+
+    print(EC2server.ping())
